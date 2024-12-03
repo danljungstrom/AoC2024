0f8a6905b4da0565b3d88c77d00f47db0b48d883
Dan Ljungström <dan.ljungstrom@aritco.com>
2024-12-03 12:46:55 +0100

    Day 3 solution

---
+++ src/day03/solution.js
@@ -0,0 +1,18 @@
+try {
+    const data = require('fs').readFileSync('src/day03/input.txt', 'utf8');
+    const parts = [1, 2];
+
+    for (part of parts) {
+        let isEnabled = true;
+        let sum = 0;
+        data.replace(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g, (match, a, b) => {
+            if (match === 'do()' && part === 2) isEnabled = true;
+            else if (match === "don't()" && part === 2) isEnabled = false;
+            else if (isEnabled && Number(a) && Number(b)) sum += Number(a) * Number(b);
+        });
+        
+    console.log("The result for part", part, "is:", sum);
+    }
+} catch (e) {
+    console.error(e);
+}
