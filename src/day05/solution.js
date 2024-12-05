51b115bcb4e5b1aabc40453b694d5e0a5fb85427
Dan Ljungström <dan.ljungstrom@aritco.com>
2024-12-05 12:45:33 +0100

    Day 5 cleanup

--- src/day05/solution.js
+++ src/day05/solution.js
@@ -1,14 +1,8 @@
 const data = require('fs').readFileSync('src/day05/input.txt', 'utf8').trim().replace(/\r\n/g, '\n');
-const sections = data.split("\n\n");
+const [rulesSection, updatesSection] = data.split("\n\n");
 
-const rules = sections[0].split("\n").map(rule => {
-  const [x, y] = rule.trim().split('|').map(Number);
-  return [x, y];
-});
-
-const updates = sections[1].split("\n").map(update => 
-  update.trim().split(',').map(Number)
-);
+const rules = rulesSection.split("\n").map(line => line.split('|').map(Number));
+const updates = updatesSection.split("\n").map(line => line.split(',').map(Number));
 
 let totalMiddleSum = 0;
 let invalidUpdates = [];
@@ -23,36 +17,41 @@ updates.forEach(update => {
 
 console.log(`The result for part 1 is: ${totalMiddleSum}`);
 
-invalidUpdatesSum = 0;
-for(const update of invalidUpdates){
-    invalidUpdatesSum += findMiddlePage(orderInvalidUpdate(update, rules));
-}
+const invalidUpdatesSum = invalidUpdates.reduce(
+    (sum, update) => sum + findMiddlePage(fixInvalidUpdate(update, rules)), 0
+);
 
 console.log(`The result for part 2 is: ${invalidUpdatesSum}`);
 
 function isUpdateValid(update, rules) {
     for (const [x, y] of rules) {
-        if (update.includes(x) && update.includes(y)) {
-            if (update.indexOf(x) > update.indexOf(y)) {
-                return false;
-            }
+        if (checkRuleBreak(update, x, y)) {
+            return false;
         }
     }
     return true;
 }
 
-function orderInvalidUpdate(update, rules) {
+function fixInvalidUpdate(invalidUpdate, rules) {
+    const update = [...invalidUpdate];
     while(!isUpdateValid(update, rules)){
         for(const [x, y] of rules){
-            if (update.includes(x) && update.includes(y)) {
-                if (update.indexOf(x) > update.indexOf(y)) {
-                    update[update.indexOf(x)] = y;
-                    update[update.indexOf(y)] = x;
-                }
+            if(checkRuleBreak(update, x, y)){
+                update[update.indexOf(x)] = y;
+                update[update.indexOf(y)] = x;
             }
         }
     }
     return update;
+}
+
+function checkRuleBreak(update, x, y) {
+    if (update.includes(x) && update.includes(y)) {
+        if (update.indexOf(x) > update.indexOf(y)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function findMiddlePage(update) {
